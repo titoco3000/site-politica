aae4f4c22dc30a4e302ff57bb1773fa42f950f91
unknown <tito.guidotti@gmail.com>
2018-05-04 15:01:22 -0300

    add o shortcode de ver os que concordam

--- functions.php
+++ functions.php
@@ -4,4 +4,26 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+function listaQconcorda($atts){
+	$atts = shortcode_atts( array(
+		'id' => ''
+	), $atts, 'listaQconcorda' );
+	$retorno = '';
+	$linkVotos = 'http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterVotacaoProposicaoPorID?idProposicao='.$atts["id"];
+	echo $linkVotos;
+	$votos_vetor = wp_remote_get($linkVotos);
+	echo "<pre>";
+	print_r($votos_vetor);
+		// $retorno = '<form action = "" method="GET">
+	// 							<input type="radio" name="s_n" value="s" checked>sim<br>
+	// 							<input type="radio"name="s_n" value="n">não
+	// 							<input type="submit">
+	// 						</form>';
+	// if(isset($_GET["s_n"])){
+	// 	echo "ei!";
+	// }
+	return $retorno;
+}
+add_shortcode('concordaPorID' , 'listaQconcorda');
 ?>
