3f12fc3d68035f53d2104a6be7f09f244c29495b
unknown <tito.guidotti@gmail.com>
2018-05-17 17:59:06 -0300

    resolve problemas na func listaQconcorda

--- functions.php
+++ functions.php
@@ -9,29 +9,73 @@ function my_theme_enqueue_styles() {
 
 function listaQconcorda($atts){
 	$atts = shortcode_atts( array(
-		'id' => ''
+		'id' => '',
+    'opiniao'=>''
 	), $atts, 'listaQconcorda' );
 	$retorno = '';
+  $objetoVotos = array('politico' => 'voto');
 	$linkVotos = 'https://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterVotacaoProposicaoPorID?idProposicao='.$atts["id"];
 	echo $linkVotos;
 	$votos_vetor = wp_remote_get($linkVotos);
   $xml = simplexml_load_string($votos_vetor['body']);
   $json = json_encode($xml);
   $array = json_decode($json,TRUE);
+
+  echo "<br>opiniao: ";  //trata da opinião
+  switch ($atts['opiniao']) {
+    case 's':
+      $opiniaoMultiplier = 1;
+      echo "Sim";
+      break;
+
+    case 'n':
+      $opiniaoMultiplier = -1;
+      echo "Não";
+      break;
+
+    default:
+      $opiniaoMultiplier = 0;
+      echo "Dê uma opinião!";
+      break;
+  }
+
+  echo"<br>";
   echo "<pre>";
-  print_r($array);
+
+  foreach ($array['Votacoes'] as $key => $value) {
+
+    foreach ($value['votos']['Deputado'] as $deputado => $values) {
+
+      $politico = $values ['@attributes']['Nome'];
+      $voto = $values ['@attributes']['Voto'];
+      if(isset($objetoVotos[$politico])){
+
+      }
+      else{
+        $objetoVotos[$politico] = 0;
+      }
+      $voto = preg_replace('/\s+/', '', $voto);   //retira whitespaces
+      switch ($voto) {
+        case 'Sim':
+          $num = 1;
+          break;
+        case "Não":
+          $num = -1;
+          break;
+
+        default:
+          $num = 0;
+          break;
+      }
+
+      $objetoVotos[$politico] = $objetoVotos[$politico] + ($num * $opiniaoMultiplier) ;
+
+    }
+
+  }
+  print_r($objetoVotos);
   echo "</pre>";
-  // echo "<pre>";
-	// print_r(esc_html($votos_vetor['body']));
-  // echo "</pre>";
-		// $retorno = '<form action = "" method="GET">
-	// 							<input type="radio" name="s_n" value="s" checked>sim<br>
-	// 							<input type="radio"name="s_n" value="n">não
-	// 							<input type="submit">
-	// 						</form>';
-	// if(isset($_GET["s_n"])){
-	// 	echo "ei!";
-	// }
+
 	return $retorno;
 }
 add_shortcode('concordaPorID' , 'listaQconcorda');
